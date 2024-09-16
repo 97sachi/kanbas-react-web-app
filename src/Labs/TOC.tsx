de3f346c62b7fa6664af95514cc658d21b59a5a4
97sachi <156970519+97sachi@users.noreply.github.com>
2024-09-16 19:55:44 -0400

    till mon

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -8,6 +8,8 @@ export default function TOC() {
         <li><Link to="/Labs/Lab2">Lab 2</Link></li>
         <li><Link to="/Labs/Lab3">Lab 3</Link></li>
         <li><Link to="/Kanbas">Kanbas</Link></li>
+        <li><a id="wd-github" href="https://github.com/97sachi/kanbas-react-web-app"> GitHub Repo Link</a></li>
+        
 
       </ul>
     );
