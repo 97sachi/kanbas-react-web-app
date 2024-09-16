39f410c7248d2d1dbd2f7d14583dfd8d09671cb0
97sachi <156970519+97sachi@users.noreply.github.com>
2024-09-16 19:34:30 -0400

    till mon

--- src/Kanbas/Courses/Home/index.tsx
+++ src/Kanbas/Courses/Home/index.tsx
@@ -2,6 +2,13 @@ import Modules from "../Modules";
 import CourseStatus from "./Status";
 export default function Home() {
   return (
+    <><button id="wd-collapse-all">Collapse All</button>
+    <button id="wd-view-progress">View Progress</button>
+    <select id="wd-publish-all">
+        <option value="ASSIGNMENTS">Publish All</option>
+        <option value="QUIZZES">Publish Selected</option>
+      </select>
+    <button id="wd-view-progress">+ Module</button>
     <table id="wd-home">
       <tr>
         <td valign="top">
@@ -11,7 +18,7 @@ export default function Home() {
           <CourseStatus />
         </td>
       </tr>
-    </table>
+    </table></>
   );
 }
 
