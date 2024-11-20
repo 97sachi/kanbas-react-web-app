4bc76dc0235c728bf83c59bae9d17181be232194
97sachi <156970519+97sachi@users.noreply.github.com>
2024-11-19 22:19:58 -0500

    a5

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -48,6 +48,11 @@ export default function TOC() {
           My GitHub
         </a>
       </li>
+      <li className="nav-item">
+        <a id="wd-k" href="https://kanbas-node-server-app-eqp9.onrender.com" className="nav-link">
+          Render Server Link 
+        </a>
+      </li>
     </ul>
     );
   }
